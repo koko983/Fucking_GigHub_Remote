4f3bbff3deaab5344788fe5cad05494a5c70228b
almowafy <abdulrahmanalmowafy44@gmail.com>
2017-12-11 03:34:51 +0200

    initial commit

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        public bool IsCanceled { get; private set; }
+
+        public ApplicationUser Artist { get; set; }
+
+        [Required]
+        public string ArtistId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+
+        public ICollection<Attendance> Attendances { get; private set; }
+
+        public Gig()
+        {
+            Attendances = new Collection<Attendance>();
+        }
+
+        public void Cancel()
+        {
+            this.IsCanceled = true;
+
+            var notification = Notification.GigCanceled(this);
+
+            var attendees = Attendances
+                .Select(a => a.Attendee)
+                .ToList();
+
+            foreach (var attendee in attendees)
+            {
+                attendee.Notify(notification);
+            }
+        }
+    }
+}
---
+++ GigHub/Models/Notification.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public NotificationType Type { get; set; }
+        public DateTime? OriginalDateTime { get; set; }
+        public string OriginalVenue { get; set; }
+
+        [Required]
+        public Gig Gig { get; set; }
+
+        protected Notification()
+        {
+
+        }
+
+        private Notification(Gig gig, NotificationType type)
+        {
+            if (gig == null)
+            {
+                throw new ArgumentNullException("gig");
+            }
+
+            Type = type;
+            Gig = gig;
+            DateTime = DateTime.Now;
+        }
+
+        public static Notification GigCanceled(Gig gig)
+        {
+            return new Notification(gig, NotificationType.GigCanceled);
+        }
+    }
+}
